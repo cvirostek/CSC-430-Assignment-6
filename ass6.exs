1e9773060d5b85b36b2085d890a1bd66a309cfd1
cvirostek <cwvirostek@gmail.com>
2019-05-27 22:03:12 -0700

    Finished interp

--- ass6.exs
+++ ass6.exs
@@ -1,6 +1,9 @@
+ExUnit.start()
+
 defmodule IdC do
     defstruct [:s]
 end
+
 defmodule NumC do
     defstruct [:n]
 end
@@ -33,22 +36,75 @@ defmodule ClosV do
     defstruct [:param, :body, :env]
 end
 
+defmodule StringV do
+    defstruct [:str]
+end
+
+defmodule NumV do
+    defstruct [:n]
+end
+
 defmodule Main do
-    def interp(exp = %_{}, env = %{}) do
+    # exp: expression
+    # env: environment
+    # returns: expression
+    def interp(exp, env) do
         case exp do
             %IdC{s: s} ->
-                IO.puts s
+                env[s]
             %NumC{n: n} ->
-                IO.puts n
-	    %StringC{str: str} ->
-		IO.puts str
+                %NumV{n: n}
+            %StringC{str: str} ->
+                %StringV{str: str}
+            %IfC{test: test, th: th, el: el} ->
+                case interp(test, env) do
+                    %BoolV{b: b} ->
+                        if b do
+                            interp(th, env)
+                        else
+                            interp(el, env)
+                        end
+                end
+            %LamC{param: param, body: body} ->
+                %ClosV{param: param, body: body, env: env}
+            %AppC{fun: fun, args: args} ->
+                case interp(fun, env) do
+                    %PrimV{p: p} ->
+                        IO.puts "TODO: prim handler"
+                    %ClosV{param: param, body: body, env: env2} ->
+                        args_eval = Enum.map(args, fn arg -> interp(arg, env) end)
+                        interp(body, extend_env(env2, param, args_eval))
+                end
         end
     end
 
+    # env: environment
+    # symbols: list of symbols
+    # values: list of values
+    # returns: environment
+    def extend_env(env, symbols, values) do
+        List.foldl(Enum.zip(symbols, values), env,
+            fn kv, acc ->
+                Map.put(acc, elem(kv, 0), elem(kv, 1))
+            end)
+    end
+
     def main do
-        interp(%IdC{s: :a}, %{})
-        interp(%NumC{n: 1}, %{})
-        interp(%StringC{str: "test"}, %{})
+        interp(%AppC{fun: %LamC{param: [:x, :y], body: %IdC{s: :y}}, args: [%NumC{n: 1}, %NumC{n: 2}]}, %{})
+    end
+end
+
+defmodule Tests do
+    use ExUnit.Case
+
+    test "interp" do
+        result = Main.interp(%AppC{fun: %LamC{param: [:x, :y], body: %IdC{s: :y}}, args: [%NumC{n: 1}, %NumC{n: 2}]}, %{})
+        assert result == %NumV{n: 2}
+    end
+
+    test "extend_env" do
+        result = Main.extend_env(%{a: %NumV{n: 0}}, [:b, :c, :d], [%NumV{n: 1}, %NumV{n: 2}, %NumV{n: 3}])
+        assert result == %{a: %NumV{n: 0}, b: %NumV{n: 1}, c: %NumV{n: 2}, d: %NumV{n: 3}}
     end
 end
 
